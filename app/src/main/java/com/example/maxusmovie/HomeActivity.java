87baa4908963b1108cd8ebbfede0f927e6862a42
harshithreddypeddolla <harshithreddypeddolla123@gmail.com>
2020-03-08 14:14:25 -0400

    partners created

--- app/src/main/java/com/example/maxusmovie/HomeActivity.java
+++ app/src/main/java/com/example/maxusmovie/HomeActivity.java
@@ -2,13 +2,26 @@ package com.example.maxusmovie;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
 
 public class HomeActivity extends AppCompatActivity {
-
+ Button search;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_home);
+        search = findViewById(R.id.button3);
+        search.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent i=new Intent(HomeActivity.this,partner.class);
+                startActivity(i);
+            }
+        });
+
+
     }
 }
